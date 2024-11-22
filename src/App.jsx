1fa977472f9e46564ea466b9b3c68e1a3a584dd1
David Rivadeneyra <david.rivadeneyra.c@gmail.com>
2024-11-22 12:50:54 -0500

    tercera seccion y correccion de padding en el botton

--- src/App.jsx
+++ src/App.jsx
@@ -51,12 +51,12 @@ function App() {
 
 		
 	</main>
-	<main className="section-gradiente-top main-background">
-			<section className="main-inner-section">
-				<div className=''>
+	<main className="main-background">
+		<div className='w-full section-gradiente-top'>
+			<section className="main-inner-section pb-40">
 					<h1 className="title-h2 title-color pt-24 pb-20 text-center">Roadmap</h1>
-					<h3 className='title-h4 paragraph-orange-500 pb-2'>Stage 1</h3>
-					<span className='paragraph-primary paragraph-orange-200 uppercase pb-8 block'>Release Date: October 11, 2024</span>
+					<h3 className='title-h4 paragraph-orange-500 pb-2'>Stage 1:</h3>
+					
 					<h2 className='title-h3 title-color pb-8 md:pb-16'>Foundations of Survival</h2>
 					
 					<div className='path-section flex flex-col-reverse md:grid md:grid-cols-12 gap-8 md:gap-16'>
@@ -64,40 +64,82 @@ function App() {
 							<div className='path-item'>
 								<span className='uppercase paragraph-orange-500 paragraph-medium pb-2'>Patch 1.0</span>
 								<h3 className='title-h4 title-color pb-4'>First Blood</h3>
+								<span className='paragraph-medium title-color block'>Release Date: October 11, 2024</span>
 								<p className="paragraph-primary paragraph-color ">Initial release, core zombie AI (Wretches), and basic player movement/combat.</p>
 							</div>
 							<div className='path-item'>
 								<span className='uppercase paragraph-orange-500 paragraph-medium pb-2'>Patch 1.1</span>
 								<h3 className='title-h4 title-color pb-4'>Adapt or Die</h3>
+								<span className='paragraph-medium title-color block'>Release Date: October 25, 2024</span>
 								<p className="paragraph-primary paragraph-color ">Improved zombie AI, pathfinding, and player inventory system.</p>
 							</div>
 							<div className='path-item'>
 								<span className='uppercase paragraph-orange-500 paragraph-medium pb-2'>Patch 1.2</span>
 								<h3 className='title-h4 title-color pb-4'>Breaking Point</h3>
+								<span className='paragraph-medium title-color block'>Release Date: November 8, 2024</span>
 								<p className="paragraph-primary paragraph-color ">Zombie variants (fast and tough Wretches) and ranged weapons for players.</p>
 							</div>
 							<div className='path-item'>
 								<span className='uppercase paragraph-orange-500 paragraph-medium pb-2'>Patch 1.3</span>
 								<h3 className='title-h4 title-color pb-4'>Defensive Measures</h3>
+								<span className='paragraph-medium title-color block'>Release Date: November 22, 2024</span>
 								<p className="paragraph-primary paragraph-color ">Player traps, simple crafting, and early progression mechanics.</p>
 							</div>
 						</div>
 						<div className='path-image col-span-9 relative visible'>
-
 							<img src="/assets/img/02.jpg"  className='aspect-ratio-img absolute blur-2xl top-0 w-full z-0' alt="" />
 							<img src="/assets/img/02.jpg" className='aspect-ratio-img relative rounded-3xl' alt="" />
 						</div>
-						
-
 					</div>
 
+			</section>
+		</div>
+			
+
+			{/* PATH 2 */}
+		<div className='w-full section-gradiente-bottom'>
+			<section className="main-inner-section pb-40">
+				<div className=''>
+					<h3 className='title-h4 paragraph-orange-500 pb-2'>Stage 2:</h3>
 					
-
-
+					<h2 className='title-h3 title-color pb-8 md:pb-16'>The Hunter’s Call</h2>
 					
-
+					<div className='path-section flex flex-col-reverse md:grid md:grid-cols-12 gap-8 md:gap-16'>
+						<div className='path-list flex flex-col gap-10 col-span-3'>
+							<div className='path-item'>
+								<span className='uppercase paragraph-orange-500 paragraph-medium pb-2'>Patch 2.0</span>
+								<h3 className='title-h4 title-color pb-4'>Rise of the Hunters</h3>
+								<span className='paragraph-medium title-color block'>Release Date: December 6, 2024</span>
+								<p className="paragraph-primary paragraph-color ">Hunters evolve from Wretches, introducing kill-based progression.</p>
+							</div>
+							<div className='path-item'>
+								<span className='uppercase paragraph-orange-500 paragraph-medium pb-2'>Patch 2.1</span>
+								<h3 className='title-h4 title-color pb-4'>Silent Stalkers</h3>
+								<span className='paragraph-medium title-color block'>Release Date: December 20, 2024</span>
+								<p className="paragraph-primary paragraph-color ">Enhanced tracking for Hunters, player stats, and leaderboards.</p>
+							</div>
+							<div className='path-item'>
+								<span className='uppercase paragraph-orange-500 paragraph-medium pb-2'>Patch 2.2</span>
+								<h3 className='title-h4 title-color pb-4'>The Hunt Begins</h3>
+								<span className='paragraph-medium title-color block'>Release Date: January 3, 2025</span>
+								<p className="paragraph-primary paragraph-color ">Advanced ranged combat for players, with improved tactical AI for Hunters.</p>
+							</div>
+							<div className='path-item'>
+								<span className='uppercase paragraph-orange-500 paragraph-medium pb-2'>Patch 2.3</span>
+								<h3 className='title-h4 title-color pb-4'>Echoes of Survival</h3>
+								<span className='paragraph-medium title-color block'>Release Date: January 17, 2025</span>
+								<p className="paragraph-primary paragraph-color ">Event system implementation, with real-time notifications for key zombie/player actions.</p>
+							</div>
+						</div>
+						<div className='path-image col-span-9 relative visible'>
+							<img src="/assets/img/01.jpg"  className='aspect-ratio-img absolute blur-2xl top-0 w-full z-0' alt="" />
+							<img src="/assets/img/01.jpg" className='aspect-ratio-img relative rounded-3xl' alt="" />
+						</div>
+					</div>
 				</div>
 			</section>
+		</div>
+			
 
 		
 	</main>
