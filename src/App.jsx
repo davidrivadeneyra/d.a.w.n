f2dece9cc063b9a27fee7df346ba5a2510f0ecde
David Rivadeneyra <david.rivadeneyra.c@gmail.com>
2024-11-22 12:14:15 -0500

    ajusando espaciados

--- src/App.jsx
+++ src/App.jsx
@@ -24,9 +24,9 @@ function App() {
 			</header>
 			<section className="main-inner-section">
 				<div className='lg:w-[530px] '>
-					<h1 className="title-h1 pb-3 lg:pb-8 pt-24"><span className='title-h2 block'>Survive the</span>  Darkness</h1>
-					<p className="paragraph-primary pb-8">D.A.W.N: Darkness Awaits Withstand the Night is a fast-paced survival game where players face off against a relentless, evolving threat of zombies and reanimated former teammates in an open-ended simulation.</p>
-					<h2 className='title-h4 pb-6'>Be the First to Know!</h2>
+					<h1 className="title-h1 title-color pb-3 lg:pb-8 pt-24"><span className='title-h2 block '>Survive the</span>  Darkness</h1>
+					<p className="paragraph-primary paragraph-color pb-8">D.A.W.N: Darkness Awaits Withstand the Night is a fast-paced survival game where players face off against a relentless, evolving threat of zombies and reanimated former teammates in an open-ended simulation.</p>
+					<h2 className='title-h4 title-color pb-4'>Be the First to Know!</h2>
 
 					<form className='flex flex-col lg:flex-row gap-4 mb-4'>
 						<div className="relative h-12 lg:h-14 w-full md:w-64 rounded-lg bg-white/[0.08] hover:bg-white/[0.1] border border-white/10  flex flex-col px-4 py-2 gap-1 cursor-pointer">
@@ -54,32 +54,32 @@ function App() {
 	<main className="section-gradiente-top main-background">
 			<section className="main-inner-section">
 				<div className=''>
-					<h1 className="title-h2 pt-24 pb-20 text-center">Roadmap</h1>
-					<h3 className='title-h4 text-[#FAA275] pb-2'>Stage 1</h3>
-					<span className='uppercase title-color paragraph-font pb-8 block'>Release Date: October 11, 2024</span>
-					<h2 className='title-h3 pb-16'>Foundations of Survival</h2>
+					<h1 className="title-h2 title-color pt-24 pb-20 text-center">Roadmap</h1>
+					<h3 className='title-h4 paragraph-orange-500 pb-2'>Stage 1</h3>
+					<span className='paragraph-primary paragraph-orange-200 uppercase pb-8 block'>Release Date: October 11, 2024</span>
+					<h2 className='title-h3 title-color pb-8 md:pb-16'>Foundations of Survival</h2>
 					
-					<div className='path-section flex flex-col-reverse md:grid md:grid-cols-12 gap-16'>
+					<div className='path-section flex flex-col-reverse md:grid md:grid-cols-12 gap-8 md:gap-16'>
 						<div className='path-list flex flex-col gap-10 col-span-3'>
 							<div className='path-item'>
-								<span className='uppercase text-[#FAA275] paragraph-font-medium pb-2'>Patch 1.0</span>
-								<h3 className='title-h4 pb-4'>First Blood</h3>
-								<p className="paragraph-primary">Initial release, core zombie AI (Wretches), and basic player movement/combat.</p>
+								<span className='uppercase paragraph-orange-500 paragraph-medium pb-2'>Patch 1.0</span>
+								<h3 className='title-h4 title-color pb-4'>First Blood</h3>
+								<p className="paragraph-primary paragraph-color ">Initial release, core zombie AI (Wretches), and basic player movement/combat.</p>
 							</div>
 							<div className='path-item'>
-								<span className='uppercase text-[#FAA275] paragraph-font-medium pb-2'>Patch 1.1</span>
-								<h3 className='title-h4 pb-4'>Adapt or Die</h3>
-								<p className="paragraph-primary">Improved zombie AI, pathfinding, and player inventory system.</p>
+								<span className='uppercase paragraph-orange-500 paragraph-medium pb-2'>Patch 1.1</span>
+								<h3 className='title-h4 title-color pb-4'>Adapt or Die</h3>
+								<p className="paragraph-primary paragraph-color ">Improved zombie AI, pathfinding, and player inventory system.</p>
 							</div>
 							<div className='path-item'>
-								<span className='uppercase text-[#FAA275] paragraph-font-medium pb-2'>Patch 1.2</span>
-								<h3 className='title-h4 pb-4'>Breaking Point</h3>
-								<p className="paragraph-primary">Zombie variants (fast and tough Wretches) and ranged weapons for players.</p>
+								<span className='uppercase paragraph-orange-500 paragraph-medium pb-2'>Patch 1.2</span>
+								<h3 className='title-h4 title-color pb-4'>Breaking Point</h3>
+								<p className="paragraph-primary paragraph-color ">Zombie variants (fast and tough Wretches) and ranged weapons for players.</p>
 							</div>
 							<div className='path-item'>
-								<span className='uppercase text-[#FAA275] paragraph-font-medium pb-2'>Patch 1.3</span>
-								<h3 className='title-h4 pb-4'>Defensive Measures</h3>
-								<p className="paragraph-primary">Player traps, simple crafting, and early progression mechanics.</p>
+								<span className='uppercase paragraph-orange-500 paragraph-medium pb-2'>Patch 1.3</span>
+								<h3 className='title-h4 title-color pb-4'>Defensive Measures</h3>
+								<p className="paragraph-primary paragraph-color ">Player traps, simple crafting, and early progression mechanics.</p>
 							</div>
 						</div>
 						<div className='path-image col-span-9 relative visible'>
